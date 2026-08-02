5432fbdac40e1945e7c247bef70f8f6cb525d087
Your Name <monu.aniket@gmail.com>
2026-08-02 21:48:18 +0530

    app added

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 from tools.tavily_tool import tavily_search
 from tools.flight_tool import search_flights
+from backend import run_travel_agent
 
 #res = tavily_search("Best hotels in india")
-res = search_flights("plan 7 days trip nepal from bangladesh")
+#res = search_flights("plan 7 days trip nepal from bangladesh")
 
-print(res)
+test = input("Enter your travel query: ")
+res = run_travel_agent(
+    user_input=test,
+    thread_id='test_thread',
+)   
+
+
+
+print("Final response from agents:")
+print(res['answer'])
